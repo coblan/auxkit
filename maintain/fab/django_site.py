4cc99fb6bf38fed5c64b08b2e0d2e9a2dad63c6b
coblan <coblan@163.com>
2022-12-12 01:18:35 +0800

    auto commit

--- maintain/fab/django_site.py
+++ maintain/fab/django_site.py
@@ -1,3 +1,5 @@
+from auxkit.maintain.fab.copy import big_remote_copy
+
 class DjangoSite(object):
     def __init__(self,server,project_name,uwsgi,nginx):
         self.server = server
@@ -18,10 +20,18 @@ class DjangoSite(object):
             self.server.run(f'ln -s /pypro/{self.project_name}/deploy/{self.nginx} {self.nginx}')
     def initSiteDir(self):
         print(f'创建{self.project_name}的必要文件夹')
+        self.server.run(f'mkdir /pypro/{self.project_name}')
         with self.server.cd(f'/pypro/{self.project_name}'):
             self.server.run(f'mkdir run')  
             self.server.run(f'mkdir log')
     
+    def copyFile(self,src_server,src_password):
+        big_remote_copy(src_server, f'/pypro/{self.project_name}', self.server, f'/pypro/{self.project_name}', src_password)
+    
+    def copyMedia(self,src_server,src_password):
+        print(f'创建{self.project_name}的media文件')
+        big_remote_copy(src_server, f'/pypro/{self.project_name}/media', self.server, f'/pypro/{self.project_name}/media', src_password)
+        
     def importDb(self):
         pass
     
