2d36cba9aeb0de727a7abdecbb6720a90508d652
coblan <coblan@163.com>
2023-03-29 00:36:43 +0800

    ggh

--- maintain/fab/django_site.py
+++ maintain/fab/django_site.py
@@ -143,8 +143,9 @@ class DjangoSite(object):
         else:
             self.server.run(f'docker stop {self.project_name}')
             
-    def start(self):
+    def start(self,uwsgi):
         self.server.run(f'docker start {self.project_name}')
+        self.server.run(f'docker exec {self.project_name} /pypro/p3dj11/bin/uwsgi /pypro/{self.project_name}/deploy/{uwsgi}')
     
     def startCelery(self,autoscale='10,3',soft_time_limit=None,sudo=None,worker='worker',queue=None):#
         """
